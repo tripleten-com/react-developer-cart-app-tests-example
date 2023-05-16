9938cce8ce07148688f4b5dafa74e42d4a666292
e-statsenko <114163693+e-statsenko@users.noreply.github.com>
2023-05-16 14:47:23 +0300

    Update postponed.jsx
    added eng

--- src/components/cart/postponed.jsx
+++ src/components/cart/postponed.jsx
@@ -4,11 +4,11 @@ import styles from './postponed.module.css';
 export const Postponed = () => {
   return (
     <div className={`${styles.postponed}`}>
-      <p className={styles.text}>Здесь пусто.</p>
+      <p className={styles.text}>it's empty here.</p>
       <p className={styles.text}>
-        Вы можете добавить товары в список отложенных, перетащив их карточку из корзины сюда.
+        You can add items to favorites by dragging the product card from the cart to here.
       </p>
-      <p className={styles.text}>Положить их обратно в корзину можно аналогичным способом.</p>
+      <p className={styles.text}>You can put them back to the cart in the same way.</p>
     </div>
   );
 };
