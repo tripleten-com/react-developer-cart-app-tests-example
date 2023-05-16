5ec1eea22c41d93b4246dd7820d478a301430de3
e-statsenko <114163693+e-statsenko@users.noreply.github.com>
2023-05-16 14:52:52 +0300

    Update tabs.jsx
    added eng

--- src/components/cart/tabs.jsx
+++ src/components/cart/tabs.jsx
@@ -8,8 +8,8 @@ export const Tabs = () => {
 
   return (
     <div className={`${styles.tabs}`}>
-      <Tab text="Товары в корзине" active={currentTab === 'items'} />
-      <Tab text="Отложенные товары" active={currentTab === 'postponed'} />
+      <Tab text="Shopping cart" active={currentTab === 'items'} />
+      <Tab text="Favorites" active={currentTab === 'postponed'} />
     </div>
   );
 };
