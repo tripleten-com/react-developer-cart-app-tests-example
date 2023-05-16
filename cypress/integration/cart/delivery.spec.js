d7f116bb792c28aa7fd66de6154c6dd21b08059e
e-statsenko <114163693+e-statsenko@users.noreply.github.com>
2023-05-16 14:38:07 +0300

    Update delivery.spec.js
    added eng

--- cypress/integration/cart/delivery.spec.js
+++ cypress/integration/cart/delivery.spec.js
@@ -1,7 +1,7 @@
 describe('delivery page display correctly', function() {
   before(function() {
     cy.visit('http://localhost:3000');
-    cy.get('button').contains('Продолжить оформление').click();
+    cy.get('button').contains('Continue to shipping').click();
   });
 
   it('should highlight selected type of delivery', function() {
@@ -11,7 +11,7 @@ describe('delivery page display correctly', function() {
     cy.get('@expressDelivery').should('have.css', 'background-color', 'rgb(250, 250, 250)');
     cy.get('@defaultDelivery').should('not.have.css', 'background-color', 'rgb(250, 250, 250)');
 
-    cy.get('@defaultDelivery').contains('Обычная доставка').click();
+    cy.get('@defaultDelivery').contains('Standard shipping').click();
 
     cy.get('@expressDelivery').should('not.have.css', 'background-color', 'rgb(250, 250, 250)');
     cy.get('@defaultDelivery').should('have.css', 'background-color', 'rgb(250, 250, 250)');
